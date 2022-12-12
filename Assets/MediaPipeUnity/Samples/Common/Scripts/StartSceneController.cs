cd858dd25582cad9da6b97f57443390a1bd50ea0
Günther Eder <gue@geder.at>
2022-12-12 07:37:10 +0100

    build works

--- Assets/MediaPipeUnity/Samples/Common/Scripts/StartSceneController.cs
+++ Assets/MediaPipeUnity/Samples/Common/Scripts/StartSceneController.cs
@@ -8,6 +8,9 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Mediapipe.Unity
 {
@@ -20,6 +23,8 @@ namespace Mediapipe.Unity
 
     private IEnumerator Start()
     {
+      LoadLibrary();
+
       var _ = Instantiate(_consolePrefab, _screen.transform);
 
       var bootstrap = GetComponent<Bootstrap>();
@@ -32,5 +37,52 @@ namespace Mediapipe.Unity
       var sceneLoadReq = SceneManager.LoadSceneAsync(1);
       yield return new WaitUntil(() => sceneLoadReq.isDone);
     }
+
+    public void LoadLibrary()
+    {
+#if UNITY_EDITOR_LINUX
+    var path = Path.Combine("Packages", "com.github.homuler.mediapipe", "Runtime", "Plugins", "libmediapipe_c.so");
+#elif UNITY_STANDALONE_LINUX
+    var path = Path.Combine(Application.dataPath, "Plugins", "libmediapipe_c.so");
+#elif UNITY_EDITOR_OSX
+      var path = Path.Combine("Packages", "com.github.homuler.mediapipe", "Runtime", "Plugins", "libmediapipe_c.dylib");
+#elif UNITY_STANDALONE_OSX || UNITY_IOS
+    var path = Path.Combine(Application.dataPath, "Plugins", "libmediapipe_c.dylib");
+#endif
+
+      var handle = dlopen(path, 2);
+
+      if (handle != IntPtr.Zero)
+      {
+        // Success
+        var result = dlclose(handle);
+
+        if (result != 0)
+        {
+          Debug.LogError($"Failed to unload {path}");
+        }
+      }
+      else
+      {
+        Debug.LogError($"Failed to load {path}: {Marshal.GetLastWin32Error()}");
+        var error = Marshal.PtrToStringAnsi(dlerror());
+        // TODO: release memory
+
+        if (error != null)
+        {
+          Debug.LogError(error);
+        }
+      }
+    }
+
+    [DllImport("dl", SetLastError = true, ExactSpelling = true)]
+    private static extern IntPtr dlopen(string name, int flags);
+
+    [DllImport("dl", ExactSpelling = true)]
+    private static extern IntPtr dlerror();
+
+    [DllImport("dl", ExactSpelling = true)]
+    private static extern int dlclose(IntPtr handle);
+
   }
 }
